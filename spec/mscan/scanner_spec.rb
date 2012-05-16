f23baf72b0780bf86131b9b6a1572496b750a626
Matt Dressel <matt.dressel@gmail.com>
2012-05-16 12:03:31 -0500

    Improved spec coverage for Scanner

--- spec/mscan/scanner_spec.rb
+++ spec/mscan/scanner_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Mscan::Scanner do
+
+  context 'scan' do
+    it 'should search all configured scan directories for media' do
+      expected_scan_directories = ['scan directory']
+      Mscan::Settings.should_receive(:scan_directories).and_return(expected_scan_directories)
+      Mscan::MediaDir.should_receive(:find_media_dirs).with(expected_scan_directories.first).and_return([])
+
+      Mscan::Scanner.new.scan
+    end
+  end
+
 end
